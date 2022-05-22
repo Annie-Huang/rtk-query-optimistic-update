86d82990a25c8009324e9f95363add1d344e8fff
Annie Huang <annie.hn@gmail.com>
2022-05-22 11:11:35 +1000

    Add notes for invalidation

--- src/app/services/posts.ts
+++ src/app/services/posts.ts
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export interface Post {
-  id: string
-  name: string
+  id: string;
+  name: string;
 }
 
-type PostsResponse = Post[]
+type PostsResponse = Post[];
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
@@ -13,13 +13,19 @@ export const api = createApi({
   endpoints: (build) => ({
     getPosts: build.query<PostsResponse, void>({
       query: () => 'posts',
+      // Provides a list of `Posts` by `id`.
+      // If any mutation is executed that `invalidate`s any of these tags, this query will re-run to be always up-to-date.
+      // The `LIST` id is a "virtual id" we just made up to be able to invalidate this query specifically if a new `Posts` element was added.
       providesTags: (result) =>
+        // is result available?
         result
-          ? [
+          ? // successful query
+            [
               ...result.map(({ id }) => ({ type: 'Post' as const, id })),
               { type: 'Post', id: 'LIST' },
             ]
-          : [{ type: 'Post', id: 'LIST' }],
+          : // an error occurred, but we still want to refetch this query when `{ type: 'Posts', id: 'LIST' }` is invalidated
+            [{ type: 'Post', id: 'LIST' }],
     }),
     addPost: build.mutation<Post, Partial<Post>>({
       query: (body) => ({
@@ -27,6 +33,8 @@ export const api = createApi({
         method: 'POST',
         body,
       }),
+      // Invalidates all Post-type queries providing the `LIST` id - after all, depending of the sort order,
+      // that newly created post could show up in any lists.
       invalidatesTags: [{ type: 'Post', id: 'LIST' }],
     }),
     getPost: build.query<Post, string>({
@@ -42,15 +50,17 @@ export const api = createApi({
       async onQueryStarted({ id, ...patch }, { dispatch, queryFulfilled }) {
         const patchResult = dispatch(
           api.util.updateQueryData('getPost', id, (draft) => {
-            Object.assign(draft, patch)
+            Object.assign(draft, patch);
           })
-        )
+        );
         try {
-          await queryFulfilled
+          await queryFulfilled;
         } catch {
-          patchResult.undo()
+          patchResult.undo();
         }
       },
+      // Invalidates all queries that subscribe to this Post `id` only.
+      // In this case, `getPost` will be re-run. `getPosts` *might*  rerun, if this id was under its results.
       invalidatesTags: (result, error, { id }) => [{ type: 'Post', id }],
     }),
     deletePost: build.mutation<{ success: boolean; id: number }, number>({
@@ -58,12 +68,13 @@ export const api = createApi({
         return {
           url: `posts/${id}`,
           method: 'DELETE',
-        }
+        };
       },
+      // Invalidates all queries that subscribe to this Post `id` only.
       invalidatesTags: (result, error, id) => [{ type: 'Post', id }],
     }),
   }),
-})
+});
 
 export const {
   useGetPostQuery,
@@ -71,4 +82,4 @@ export const {
   useAddPostMutation,
   useUpdatePostMutation,
   useDeletePostMutation,
-} = api
+} = api;
